d523a54ceffd299a995b68a56b11a557bcb5ac81
icp049 <ianpepe2000@gmail.com>
2024-03-01 22:33:31 -0600

    modifying requests

--- drinks/views.py
+++ drinks/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-@api_view('GET', 'POST')
+@api_view(['GET', 'POST'])
 def drink_list(request):
 
     #get all drinks
