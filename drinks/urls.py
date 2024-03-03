ebb2e769e22e25986786a8036c3347a183a1d771
icp049 <ianpepe2000@gmail.com>
2024-03-02 19:19:06 -0600

    modified @apiview

--- drinks/urls.py
+++ drinks/urls.py
@@ -20,6 +20,6 @@ from drinks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('drinks/', views.drink_list)
-    path('drinks/<int:id>', views.drink_detail)
+    path('drinks/', views.drink_list),
+    path('drinks/<int:id>', views.drink_detail),
 ]
